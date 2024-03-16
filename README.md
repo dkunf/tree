b185c69d5f54844f0d1107397372eaa5e4f452c2
doniklt <dkunfchaworl@gmail.com>
2024-03-16 17:16:35 +0200

    just documenting plans

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-To visualize info sometimes tree structure is helpful
+To visualize info sometimes tree structure is helpful.
 
 Here I tried to have declaratively described tree (in HTML),
-which is fully responsive as I did not fix any sizes
+which is fully responsive as I did not fix any sizes.
 
-and 
-
-has lines, connecting parents with children, made with js.
+It has lines, connecting parents with children, made with js dynamically.
 Lines are redrawn on window resize
-
 
 Currently depth of tree is 3 levels (lines are drawn for only 3 levels)
 
-I plan to change drawingAll function to be recursive no matter how deep is tree
+## NEXT TO DO
+
+change drawAllLines to accept any depth of tree, not just 3.
+
+lines overwrite: green line can be overwritten with blue etc, maybe we can place lines next to each other. First count children then place each line to child separately - starting from different point.
+
+maybe start from all tree hidden except root, reveal next level onclick or hover.
+This way few trees can be shown starting from top line. Their roots can be siblings then for the beginning.
--- connections.js
+++ connections.js
@@ -2,39 +2,53 @@
 document.addEventListener("DOMContentLoaded", drawAllLines);
 window.addEventListener("resize", drawAllLines);
 
-function drawLineBetween(parent, child, color = "black") {
+function drawLineBetween(parent, child, delta = 0, color = "grey") {
   setSVGHeight();
   const parentRect = parent.getBoundingClientRect();
   const childRect = child.getBoundingClientRect();
 
   const scrollY = window.scrollY || window.pageYOffset;
+  // const shift = Math.pow(-1, delta) * delta;
+  const shift = 0;
+  const parentXstart = parentRect.left + parentRect.width / 2 + shift;
+  const parentYstart = parentRect.bottom + scrollY;
+  const childXmid = childRect.left + childRect.width / 2;
 
-  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line.setAttribute("x1", parentRect.left + parentRect.width / 2);
-  line.setAttribute("y1", parentRect.bottom + scrollY);
-  line.setAttribute("x2", parentRect.left + parentRect.width / 2);
-  line.setAttribute("y2", 20 + parentRect.bottom + scrollY);
-  line.setAttribute("stroke", color);
+  const verticalStartLine = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "line"
+  );
+  verticalStartLine.setAttribute("x1", parentXstart);
+  verticalStartLine.setAttribute("y1", parentYstart);
+  verticalStartLine.setAttribute("x2", parentXstart);
+  verticalStartLine.setAttribute("y2", 20 + parentYstart);
+  verticalStartLine.setAttribute("stroke", color);
 
-  const line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line2.setAttribute("x1", parentRect.left + parentRect.width / 2);
-  line2.setAttribute("y1", 20 + parentRect.bottom + scrollY);
-  line2.setAttribute("x2", childRect.left + childRect.width / 2);
-  line2.setAttribute("y2", 20 + parentRect.bottom + scrollY);
-  line2.setAttribute("stroke", color);
+  const horizontalLine = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "line"
+  );
+  horizontalLine.setAttribute("x1", parentXstart);
+  horizontalLine.setAttribute("y1", 20 + parentYstart + shift);
+  horizontalLine.setAttribute("x2", childXmid);
+  horizontalLine.setAttribute("y2", 20 + parentYstart + shift);
+  horizontalLine.setAttribute("stroke", color);
 
-  const line3 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line3.setAttribute("x1", childRect.left + childRect.width / 2);
-  line3.setAttribute("y1", 20 + parentRect.bottom + scrollY);
-  line3.setAttribute("x2", childRect.left + childRect.width / 2);
-  line3.setAttribute("y2", childRect.top + scrollY);
-  line3.setAttribute("stroke", color);
+  const verticalEndLine = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "line"
+  );
+  verticalEndLine.setAttribute("x1", childXmid);
+  verticalEndLine.setAttribute("y1", 20 + parentYstart + shift);
+  verticalEndLine.setAttribute("x2", childXmid);
+  verticalEndLine.setAttribute("y2", childRect.top + scrollY);
+  verticalEndLine.setAttribute("stroke", color);
 
   // Append SVG lines to SVG container
   const svgContainer = document.getElementById("svg-container");
-  svgContainer.appendChild(line);
-  svgContainer.appendChild(line2);
-  svgContainer.appendChild(line3);
+  svgContainer.appendChild(verticalStartLine);
+  svgContainer.appendChild(horizontalLine);
+  svgContainer.appendChild(verticalEndLine);
 }
 
 function drawAllLines() {
@@ -43,12 +57,14 @@ function drawAllLines() {
   svgContainer.innerHTML = "";
 
   //the following 2 need to change to recursion for any depth
+  //need to rethink HTML structure to be more universal, not just main,branch,leaf
+  //or nest branches infinitely if needed
   const allMain = document.querySelectorAll(".main");
   allMain.forEach((par) => {
     let parTitle = par.querySelector(".main-title");
     let children = par.querySelectorAll(".branch-title");
-    children.forEach((child) => {
-      drawLineBetween(parTitle, child, "green");
+    children.forEach((child, ind) => {
+      drawLineBetween(parTitle, child, ind, "green");
     });
   });
 
@@ -56,8 +72,8 @@ function drawAllLines() {
   allBranches.forEach((par) => {
     let parTitle = par.querySelector(".branch-title");
     let children = par.querySelectorAll(".leaf-title");
-    children.forEach((child) => {
-      drawLineBetween(parTitle, child, "blue");
+    children.forEach((child, ind) => {
+      drawLineBetween(parTitle, child, ind, "blue");
     });
   });
 }
